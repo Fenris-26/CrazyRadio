c945a32afc360626a50809fa51e5c9098ff81456
Fenris-26 <26289562+Fenris-26@users.noreply.github.com>
2020-08-05 14:28:41 -0400

    Create test-script.sh

---
+++ test-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#This is a simple script used to determine if script injection works with a Logitech keyboard connected to a linux host.
+#This PoC takes advantage of an inherent flaw with wireless mice and keyboards that utilize a dongle for input.
+#A CrazyRadioPA and the Mousejack repositories are necessary for this to work. See ' https://github.com/insecurityofthings/jackit '
+#Made by TreyCraf7
+
+echo "this was pulled from Github."" Patch your device"
+
